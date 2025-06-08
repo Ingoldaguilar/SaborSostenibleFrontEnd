d09e4cffb1d0da8b04c05a9db60ff0449c9d8246
Geviro <101231567+Geviro@users.noreply.github.com>
2025-06-08 15:25:09 -0600

    Se creó la vista de ListVolunteerRequestsPage.xaml

---
+++ SaborSostenibleFrontEnd/Entities/VolunteerRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaborSostenibleFrontEnd.Entities
+{
+    public class VolunteerRequest
+    {
+        public int RequestId { get; set; }
+        public DateTime RequestDate { get; set; }
+        public string FirstName1 { get; set; }
+        public string FirstName2 { get; set; }
+        public string LastName1 { get; set; }
+        public string LastName2 { get; set; }
+
+        public string FullName =>
+            $"{FirstName1} {FirstName2} {LastName1} {LastName2}";
+    }
+}
