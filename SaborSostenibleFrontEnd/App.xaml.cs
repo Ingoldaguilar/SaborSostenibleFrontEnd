e9c5963b4ab875d27b0eebead9c1311b57427d5e
Geviro <101231567+Geviro@users.noreply.github.com>
2025-06-08 13:29:39 -0600

    Se creó todo lo relacionado con InsertFoodBank

--- SaborSostenibleFrontEnd/App.xaml.cs
+++ SaborSostenibleFrontEnd/App.xaml.cs
@@ -6,7 +6,7 @@
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new InsertBusinessPage());
+            MainPage = new NavigationPage(new InsertFoodBankPage());
         }
     }
 }
