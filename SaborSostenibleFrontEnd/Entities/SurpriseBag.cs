1e6a86a1a4fbebd6fc6981950031c233d47c42fb
Ingold Aguilar <83740872+Ingoldaguilar@users.noreply.github.com>
2025-06-09 17:13:32 -0600

    Primer update comprar bolsa

---
+++ SaborSostenibleFrontEnd/Entities/SurpriseBag.cs
@@ -0,0 +1,8 @@
+﻿namespace SaborSostenibleFrontEnd.Entities
+{
+    public class SurpriseBag
+    {
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
