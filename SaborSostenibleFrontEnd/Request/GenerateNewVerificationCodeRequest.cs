8cd4976b7240069ac08b891a9bb0d343f2223280
AaronVillalobosRodriguez <146499908+AaronVillalobosRodriguez@users.noreply.github.com>
2025-06-13 20:56:08 -0600

    APIs para restablecer contraseña

---
+++ SaborSostenibleFrontEnd/Request/GenerateNewVerificationCodeRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaborSostenibleFrontEnd.Request
+{
+    public class GenerateNewVerificationCodeRequest
+    {
+        public string Email { get; set; }
+    }
+}
---
+++ SaborSostenibleFrontEnd/Request/ReqResetPassword.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaborSostenibleFrontEnd.Request
+{
+    public class ReqResetPassword
+    {
+        public string Email { get; set; }
+        public string PasswordHash1 { get; set; }
+        public string PasswordHash2 { get; set; }
+        public string VerificationCode { get; set; }
+    }
+}
